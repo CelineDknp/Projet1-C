dc17261f314ba66b25de50af966ad9f84bbe6b5e
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2016-03-16 19:31:08 +0100

    main.c ?

--- main.c
+++ main.c
@@ -13,11 +13,10 @@ int main(int argc, char const *argv[]) {
 	memloc = memsize;
 	printf("Début des tests de performance\n");
 	time(&start);
-	printf("Taille allouable : %zu\n", memsize);
 	size_t temp_size = 0;
 	size_t all_size = 0;
-	void * last_pointer = 2;
-	do {
+	void * last_pointer = NULL;
+	while (all_size < memsize) {
 		temp_size = rand() % 10 + 1;
 		size_t aligned_size = temp_size - (temp_size % 4);
 		if (aligned_size < temp_size) {
@@ -26,11 +25,8 @@ int main(int argc, char const *argv[]) {
 			temp_size = aligned_size;
 		}
 		all_size = all_size + temp_size + sizeof(header);
-		if (last_pointer == NULL) {
-			break;		
-		}
 		if (all_size+temp_size >= memsize) {
-			printf("Fin des allocations\n");
+			printf("Fin des allocations, plus de mémoire\n");
 			break;
 		}
 		void * ptr = mymalloc(temp_size);
@@ -39,8 +35,11 @@ int main(int argc, char const *argv[]) {
 			myfree(last_pointer);
 		}
 		last_pointer = ptr;
-		//printf("Mémoire allouée : %zu \t Mémoire restante : %zu \t Mémoire disponible : %zu\n", all_size, memsize-all_size, memloc);
-	} while(all_size < memsize);
+		if (last_pointer == NULL) {
+			printf("Fin des allocations, pointeur null\n");
+			break;
+		}
+	}
 	time(&stop);
 	double time_of_test = stop-start;
 	printf("Temps du test : %d\n", (int) time_of_test);
